dacaef5671c39fc71791dad462108d7e5fc73c91
rodneyyyyyyyy <aaron.11.hyatt@gmail.com>
2025-11-23 19:05:13 +1100

    Add files via upload

---
+++ epicjokers /jokers/gumbly.lua
@@ -0,0 +1,86 @@
+
+SMODS.Joker{ --gumbly
+    key = "gumbly",
+    config = {
+        extra = {
+        }
+    },
+    loc_txt = {
+        ['name'] = 'gumbly',
+        ['text'] = {
+            [1] = '{C:dark_edition,E:1}Crashes the game if poker hand is your',
+            [2] = 'most played hand.{} Can only be destroyed',
+            [3] = 'by the{} {C:tarot}De-Gumblifier{}'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 0,
+        y = 1
+    },
+    display_size = {
+        w = 71 * 1.5, 
+        h = 95 * 1.5
+    },
+    cost = 15,
+    rarity = "epicjokers_epic",
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = false,
+    unlocked = true,
+    discovered = false,
+    atlas = 'CustomJokers',
+    pools = { ["epicjokers_epicjokers_jokers"] = true },
+    soul_pos = {
+        x = 1,
+        y = 1
+    },
+    in_pool = function(self, args)
+        return (
+            not args 
+            or args.source ~= 'sho' and args.source ~= 'buf' and args.source ~= 'jud' 
+            or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
+        )
+        and true
+    end,
+    
+    set_ability = function(self, card, initial)
+        card:set_eternal(true)
+    end,
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            if (function()
+                local current_played = G.GAME.hands[context.scoring_name].played or 0
+                for handname, values in pairs(G.GAME.hands) do
+                    if handname ~= context.scoring_name and values.played > current_played and values.visible then
+                        return false
+                    end
+                end
+                return true
+            end)() then
+                error("EasternFarmer Was Here")
+            end
+        end
+    end
+}
+
+local check_for_buy_space_ref = G.FUNCS.check_for_buy_space
+G.FUNCS.check_for_buy_space = function(card)
+    if card.config.center.key == "j_epicjokers_gumbly" then -- ignore slot limit when bought
+        return true
+    end
+    return check_for_buy_space_ref(card)
+end
+
+local can_select_card_ref = G.FUNCS.can_select_card
+G.FUNCS.can_select_card = function(e)
+    	if e.config.ref_table.config.center.key == "j_epicjokers_gumbly" then
+        		e.config.colour = G.C.GREEN
+        		e.config.button = "use_card"
+    	else
+        		can_select_card_ref(e)
+    	end
+end
